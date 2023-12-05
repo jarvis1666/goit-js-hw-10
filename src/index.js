33d41fa9de53659d21fccc7af4e74850678dc25f
jarvis1666 <139481899+jarvis1666@users.noreply.github.com>
2023-12-05 22:58:51 +0200

    b

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 //бібліотека для обробки селекту
+
 //npm install slim-select
 import SlimSelect from 'slim-select';
+import '/node_modules/slim-select/dist/slimselect.css';
 //npm i notiflix
+
 import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
 axios.defaults.headers.common['x-api-key'] =
   'live_0h6gWU7nqCgXTyBDNuNjMFGmNzBMJ1pY0FmHBqh87swJtLjlQOpuPp9ZMsqYbble';
 
@@ -12,10 +16,6 @@ const breedSelection = document.querySelector('select.breed-select');
 const loading = document.querySelector('.loader');
 const ERROR = document.querySelector('.error');
 const informationForCat = document.querySelector('.cat-info');
-
-new SlimSelect({
-  select: '#single',
-});
 
 breedSelection.classList.add('hidden');
 ERROR.classList.add('hidden');
@@ -34,7 +34,7 @@ fetchBreeds()
     return (breedSelection.innerHTML = selectMarkup);
   })
   .catch(error => {
-    ERROR.classList.remove('hidden');
+    // ERROR.classList.remove('hidden');
     breedSelection.classList.add('hidden');
     Notiflix.Notify.warning(
       'Oops! Something went wrong! Try reloading the page!'
@@ -46,20 +46,20 @@ breedSelection.addEventListener('change', handleSelection);
 function handleSelection(event) {
   loading.classList.add('hidden');
   breedSelection.classList.remove('hidden');
-  ERROR.classList.add('hidden');
+  //   ERROR.classList.add('hidden');
 
   const breedId = event.target.value;
   fetchCatByBreed(breedId)
     .then(catData => {
       loading.classList.add('hidden');
       breedSelection.classList.remove('hidden');
-      ERROR.classList.add('hidden');
+      //   ERROR.classList.add('hidden');
 
       const { breeds, url } = catData;
       const { description, name, temperament } = breeds[0];
       const catInfo = `
-            <img class = "fotoCat" src = "${url}" alt = "${name}" width ="400" heght ="300">
-            <div>
+            <img class = "fotoCat" src = "${url}" alt = "${name}" width ="500" heght ="400">
+            <div class = "infoCat">
                 <h1>${name}</h1>
                 <p>${description}</p>
                 <h3>${temperament}</h3>
@@ -72,7 +72,10 @@ function handleSelection(event) {
       Notiflix.Notify.warning(
         'Oops! Something went wrong! Try reloading the page!'
       );
-      ERROR.classList.remove('hidden');
+      //   ERROR.classList.remove('hidden');
       breedSelection.classList.add('hidden');
     });
 }
+new SlimSelect({
+  select: '.breed-select',
+});
